948317cba975f6c85778cad588d9dbc4dff90563
ShadeCoder707 <aidanclark5835@gmail.com>
2025-04-29 19:07:43 -0400

    feat New Robot Code

---
+++ src/main/include/Robot.h
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#ifndef ROBOT_H
+#define ROBOT_H
+
+
+#include <frc/TimedRobot.h>
+#include <frc2/command/CommandPtr.h>
+
+class Robot : public frc::TimedRobot {
+ public:
+  Robot();
+  void RobotPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void DisabledExit() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void AutonomousExit() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void TeleopExit() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+  void TestExit() override;
+
+ private:
+
+};
+
+#endif 
